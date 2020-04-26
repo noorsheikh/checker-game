06b966057459a7b3bf2367601ebca1e919221940
Brandon St. Amour <bdstamour@gmail.com>
2020-04-26 10:11:52 -0400

    Fix for authentication URL on cross platform

--- client/src/utils/index.tsx
+++ client/src/utils/index.tsx
@@ -11,4 +11,7 @@ export const dictionary = [
   '72vmin',
 ];
 
-export const dist = (x1: any, y1: any, x2: any, y2: any) => Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+export const dist = (x1: any, y1: any, x2: any, y2: any) =>
+  Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+
+export const authUrl = process.env.REACT_APP_AUTH_URL;
