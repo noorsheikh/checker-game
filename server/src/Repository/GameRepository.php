dc87e9577c2cefe09e485c5d19a792ead73bded2
Noor Sheikh <noorsheikh.me@gmail.com>
2020-05-10 18:24:30 -0400

    Update games status and forfeit games

--- server/src/Repository/GameRepository.php
+++ server/src/Repository/GameRepository.php
@@ -18,4 +18,38 @@ class GameRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Game::class);
     }
+
+    public function updateGamesStatus(\DateTimeImmutable $currentDate)
+    {
+        $conn = $this->getEntityManager()->getConnection();
+        $sql = "
+            UPDATE game
+            SET
+                `game_status` = 'completed',
+                winner_id = IF(`player_turn`=1,`player1_id`,`player2_id`)
+            WHERE game_status = 'in-progress'
+                AND updated_at IS NOT NULL
+                AND DATEDIFF(?, updated_at) > 7
+                AND player_turn != 0
+        ";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(1, $currentDate->format('Y-m-d H:i:s'));
+        return $stmt->execute();
+    }
+
+    public function deleteGames(\DateTimeImmutable $currentDate)
+    {
+        $conn = $this->getEntityManager()->getConnection();
+        $sql = "
+            DELETE game FROM game
+            WHERE game_status = 'not-started'
+                AND updated_at IS NULL
+                AND DATEDIFF(?, created_at) > 7
+        ";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(1, $currentDate->format('Y-m-d H:i:s'));
+        return $stmt->execute();
+    }
 }
